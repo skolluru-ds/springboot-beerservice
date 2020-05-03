c8ddbd517e43839ab9eb7c123a0a1e2a277a5891
skolluru-ds <funntalent@gmail.com>
2020-05-03 12:33:12 -0500

    Initial code commit. Code generated by Spring Initializer

---
+++ src/main/java/com/kolluru/example/springbootbeerservice/SpringbootBeerserviceApplication.java
@@ -0,0 +1,13 @@
+package com.kolluru.example.springbootbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBeerserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBeerserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kolluru/example/springbootbeerservice/SpringbootBeerserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kolluru.example.springbootbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBeerserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
