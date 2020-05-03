c51de949096bcdf27f08b01d215fab145192d1c5
skolluru-ds <funntalent@gmail.com>
2020-05-03 13:19:17 -0500

    Created the Controller for Get, Post, and Put methods. Closes issue #1

---
+++ src/main/java/com/kolluru/example/springbootbeerservice/controller/BeerController.java
@@ -0,0 +1,33 @@
+package com.kolluru.example.springbootbeerservice.controller;
+
+import com.kolluru.example.springbootbeerservice.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/beer")
+public class BeerController {
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId){
+       //TODO: Implementation
+       return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto) {
+        //TODO: Implementation
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PostMapping("/{beerId}")
+    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto){
+        //TODO: Implementation
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+
+}
